a9ce79c466f03f83a875a720f0ebddb071223285
anhkiet287 <kietnguyen11128@gmail.com>
2025-11-23 13:47:07 +0700

    Add src/main.c template
    Initial implementation of the matrix multiplication program with placeholders for future functionality.

---
+++ src/main.c
@@ -0,0 +1,21 @@
+// main.c
+// Entry point for matrix multiplication program
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "kernels.h"
+#include "mpi_wrapper.h"
+#include "utility.h"
+
+int main(int argc, char **argv) {
+    // TODO: Parse arguments (N, approach, algorithm...)
+    // TODO: Allocate matrices A, B, C
+    // TODO: Initialize data
+    // TODO: Choose and call appropriate function
+    // TODO: Measure time
+    // TODO: Check correctness
+    // TODO: Free memory
+    
+    return 0;
+}
